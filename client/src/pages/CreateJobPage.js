a22649610d26f745470dc19116f1e010737fbb29
Nikolay Dimov <114500017+NikolayDimov@users.noreply.github.com>
2024-03-15 15:39:32 +0200

    CreateJobPage add useCreateJob hook

--- client/src/pages/CreateJobPage.js
+++ client/src/pages/CreateJobPage.js
@@ -1,30 +1,16 @@
-import { useMutation } from "@apollo/client";
 import { useState } from "react";
 import { useNavigate } from "react-router";
-import { createJobMutation, jobByIdQuery } from "../lib/graphql/queries";
+import { useCreateJob } from "../lib/graphql/hooks";
 
 function CreateJobPage() {
     const navigate = useNavigate();
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
-    const [mutate] = useMutation(createJobMutation);
+    const { createJob, loading } = useCreateJob();
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-        // const job = await createJob({ title, description });
-        const {
-            data: { job },
-        } = await mutate({
-            variables: { input: { title, description } },
-            update: (cache, { data }) => {
-                cache.writeQuery({
-                    query: jobByIdQuery,
-                    variables: { id: data.job.id },
-                    data,
-                });
-            },
-        });
-
+        const job = await createJob(title, description);
         console.log("job created:", job);
         navigate(`/jobs/${job.id}`);
     };
@@ -48,7 +34,7 @@ function CreateJobPage() {
                     </div>
                     <div className="field">
                         <div className="control">
-                            <button className="button is-link" onClick={handleSubmit}>
+                            <button className="button is-link" disabled={loading} onClick={handleSubmit}>
                                 Submit
                             </button>
                         </div>
