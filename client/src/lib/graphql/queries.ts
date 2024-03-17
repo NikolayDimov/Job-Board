ddc161b60e4ea36ae09fa339b74fe1225cd08728
Nikolay Dimov <114500017+NikolayDimov@users.noreply.github.com>
2024-03-17 21:29:49 +0200

    client - useJobs hook typescript

--- client/src/lib/graphql/queries.ts
+++ client/src/lib/graphql/queries.ts
@@ -1,82 +1,82 @@
-import { ApolloClient, ApolloLink, concat, createHttpLink, gql, InMemoryCache } from '@apollo/client';
-import { getAccessToken } from '../auth';
+import { ApolloClient, ApolloLink, concat, createHttpLink, gql, InMemoryCache } from "@apollo/client";
+import { getAccessToken } from "../auth";
+import { graphql } from "../../generated";
 
-const httpLink = createHttpLink({ uri: 'http://localhost:9000/graphql' });
+const httpLink = createHttpLink({ uri: "http://localhost:9000/graphql" });
 
 const authLink = new ApolloLink((operation, forward) => {
-  const accessToken = getAccessToken();
-  if (accessToken) {
-    operation.setContext({
-      headers: { 'Authorization': `Bearer ${accessToken}` },
-    });
-  }
-  return forward(operation);
+    const accessToken = getAccessToken();
+    if (accessToken) {
+        operation.setContext({
+            headers: { Authorization: `Bearer ${accessToken}` },
+        });
+    }
+    return forward(operation);
 });
 
 export const apolloClient = new ApolloClient({
-  link: concat(authLink, httpLink),
-  cache: new InMemoryCache(),
+    link: concat(authLink, httpLink),
+    cache: new InMemoryCache(),
 });
 
-const jobDetailFragment = gql`
-  fragment JobDetail on Job {
-    id
-    date
-    title
-    company {
-      id
-      name
-    }
-    description
-  }
-`;
-
-export const companyByIdQuery = gql`
-  query CompanyById($id: ID!) {
-    company(id: $id) {
-      id
-      name
-      description
-      jobs {
-        id
-        date
-        title
-      }
-    }
-  }
-`;
-
-export const jobByIdQuery = gql`
-  query JobById($id: ID!) {
-    job(id: $id) {
-      ...JobDetail
-    }
-  }
-  ${jobDetailFragment}
-`;
-
-export const jobsQuery = gql`
-  query Jobs($limit: Int, $offset: Int) {
-    jobs(limit: $limit, offset: $offset) {
-      items {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const jobDetailFragment = graphql(`
+    fragment JobDetail on Job {
         id
         date
         title
         company {
-          id
-          name
+            id
+            name
         }
-      }
-      totalCount
+        description
     }
-  }
-`;
+`);
 
-export const createJobMutation = gql`
-  mutation CreateJob($input: CreateJobInput!) {
-    job: createJob(input: $input) {
-      ...JobDetail
+export const companyByIdQuery = graphql(`
+    query CompanyById($id: ID!) {
+        company(id: $id) {
+            id
+            name
+            description
+            jobs {
+                id
+                date
+                title
+            }
+        }
     }
-  }
-  ${jobDetailFragment}
-`;
+`);
+
+export const jobByIdQuery = graphql(`
+    query JobById($id: ID!) {
+        job(id: $id) {
+            ...JobDetail
+        }
+    }
+`);
+
+export const jobsQuery = graphql(`
+    query Jobs($limit: Int, $offset: Int) {
+        jobs(limit: $limit, offset: $offset) {
+            items {
+                id
+                date
+                title
+                company {
+                    id
+                    name
+                }
+            }
+            totalCount
+        }
+    }
+`);
+
+export const createJobMutation = graphql(`
+    mutation CreateJob($input: CreateJobInput!) {
+        job: createJob(input: $input) {
+            ...JobDetail
+        }
+    }
+`);
